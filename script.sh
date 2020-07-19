6843768543ac206cfe99743149af7de34816b4a1
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2020-07-19 17:11:26 -0500

    Update script_v1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {0..100};do
 	for  j in {0..49}; do
-		./bin/cliente 127.0.0.1 4535 ./files/aleatorio/archivo$j archivo$j
+		./bin/cliente 127.0.0.1 4535 ../files/aleatorio/archivo$j archivo$j
 	done
 done
